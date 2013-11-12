aac1936245d806411ef9baa59077e8bab6380265
secretrobotron <secretrobotron@gmail.com>
2013-11-12 10:10:15 -0500

    much better mashing

---
+++ components/mash.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+fs.readdir(__dirname, function (err, files) {
+  files.forEach(function (cssFilename, cssFilenameIndex) {
+    if (cssFilenameIndex > 1) return;
+    var cssFilenameMatch = cssFilename.match(/^app-([^\.]+).css$/);
+    if (cssFilenameMatch) {
+      fs.readFile(__dirname + '/' + cssFilename, 'utf8', function (cssFileErr, cssFileData) {
+        if (!cssFileErr) {
+          cssFileData = cssFileData.toString();
+          var htmlFileName = cssFilenameMatch[1] + '.html';
+          fs.readFile(__dirname + '/' + htmlFileName, 'utf8', function (htmlFileErr, htmlFileData) {
+            htmlFileData = htmlFileData.toString();
+            var templateMatch = htmlFileData.match(/<template>(\s+)/);
+            var spacing = templateMatch[1];
+            var start = templateMatch.index + templateMatch[0].length;
+            spacing = spacing.replace('\n', '');
+
+            cssFileData = '<style>\n' + spacing +
+              '  '  + cssFileData.split('\n').join('\n' + spacing + '  ') +
+              '\n' + spacing + '</style>';
+
+            htmlFileData = htmlFileData.substring(0, start) + cssFileData + '\n' + spacing + htmlFileData.substring(start);
+
+            var oldStylesheetRegexp = new RegExp('<link rel="stylesheet" href="\{\{ASSET_HOST\}\}.*' + cssFilename + '"\s*/?\s*>(</link>)?');
+
+            htmlFileData = htmlFileData.replace(oldStylesheetRegexp, '');
+
+            fs.writeFile(__dirname + '/' + htmlFileName, htmlFileData);
+          });
+        }
+      });
+    }
+  });
+});
--- components/mash.sh
+++
@@ -1,4 +0,0 @@
-for i in `ls app-*.css`; do
-  g=`echo $i | egrep -o \\\-\[^\\\.\]+ | egrep -o [^\\\-].+`;
-  cat $i >> $g.html
-done
